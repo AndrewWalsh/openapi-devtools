e7d5a966073a4c5b02edc02e98700a8d40d478a7
AndrewWalsh <softwaredevservices@pm.me>
2023-11-25 20:41:20 -0800

    remove stray comment

--- src/lib/store-helpers/find-pathnames-in-router.ts
+++ src/lib/store-helpers/find-pathnames-in-router.ts
@@ -9,10 +9,6 @@ type PathnamesLeaves = Array<{
 
 const formatPathname = (parts: Array<string>) => `/${parts.join("/").slice(1)}`;
 
-/**
- * Find matching pathnames for a given path
- * Walk the tree. Return string[][], each element of which is a path
- */
 const recurse = (
   node: RadixNode<RouteData>,
   parts: Array<string>,
@@ -46,45 +42,6 @@ const recurse = (
       pathnames.push(...recurse(child, parts.slice(1), [...walked, part]));
     }
   }
-
-  // if (matchAny) {
-  //   // If this part is a wildcard, match any
-  //   // If it's also the last part, include leaf children
-  //   // Otherwise, recurse on all children
-  //   if (isLast) {
-  //     for (const [lastPart, child] of node.children.entries()) {
-  //       if (child.data) {
-  //         const value = {
-  //           pathname: formatPathname([...walked, lastPart]),
-  //           leaf: child.data.data as Leaf,
-  //         };
-  //         pathnames.push(value);
-  //       }
-  //     }
-  //   } else {
-  //     for (const [lastPart, child] of node.children.entries()) {
-  //       pathnames.push(
-  //         ...recurse(child, parts.slice(1), [...walked, lastPart])
-  //       );
-  //     }
-  //   }
-  // } else if (node.children.has(part)) {
-  //   // If this part is static, match it
-  //   // If it's also the last part, include leaf child
-  //   // Otherwise, recurse on child
-  //   const child = node.children.get(part)!;
-  //   if (isLast) {
-  //     const value = {
-  //       pathname: formatPathname([...walked, part]),
-  //       leaf: child.data!.data as Leaf,
-  //     };
-  //     pathnames.push(value);
-  //   } else {
-  //     if (child) {
-  //       pathnames.push(...recurse(child, parts.slice(1), [...walked, part]));
-  //     }
-  //   }
-  // }
 
   return pathnames;
 };
